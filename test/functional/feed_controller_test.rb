1a0fd3891c27a0e597e807bdd50d54c4ae5d17dd
Peter Jones <pjones@pmade.com>
2007-01-31 17:31:32 +0000

    add tests for RSS feeds (that fail right now due to resource feeder)
    git-svn-id: file:///data/svn/devalot/devalot/trunk@500 3db89d08-1e44-db11-a2a8-0014220fd31b

--- test/functional/feed_controller_test.rb
+++ test/functional/feed_controller_test.rb
@@ -1,18 +1,46 @@
+################################################################################
 require File.dirname(__FILE__) + '/../test_helper'
 require 'feed_controller'
 
+################################################################################
 # Re-raise errors caught by the controller.
 class FeedController; def rescue_action(e) raise e end; end
 
+################################################################################
 class FeedControllerTest < Test::Unit::TestCase
+  ################################################################################
+  fixtures(:policies, :projects, :blogs, :articles, :tickets, :ticket_histories)
+
+  ################################################################################
   def setup
     @controller = FeedController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  ################################################################################
+  def test_all_articles
+    %W(rss atom).each do |format|
+      get("articles", {:blog => 'all', :format => format})
+      assert_response(:success)
+    end
   end
+
+  ################################################################################
+  def test_support_tickets
+    %W(rss atom).each do |format|
+      %W(all 1).each do |id|
+        get("tickets", {
+          :project => 'support', 
+          :code    => 'w00tburger', 
+          :format  => format,
+          :id      => id,
+        })
+
+        assert_response(:success)
+      end
+    end
+  end
+
 end
+################################################################################
