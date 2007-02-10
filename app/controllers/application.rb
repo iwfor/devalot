8e174315a583aa2bd3784d1fc2eb526618d55b4d
Peter Jones <pjones@pmade.com>
2007-02-10 19:08:11 +0000

    fix ticket 167: Private Project Feeds Redirecting
    git-svn-id: file:///data/svn/devalot/devalot/trunk@537 3db89d08-1e44-db11-a2a8-0014220fd31b

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -131,16 +131,18 @@ class ApplicationController < ActionController::Base
     # if a project is private, and you're not an admin, you must be a project
     # member or have the project secret code to continue.
     unless @project.public? 
+      if @project.rss_id == params[:code].to_s.downcase
+        return true
+      end
+
       unless logged_in?
         session[:after_login] = request.request_uri
         redirect_to(:controller => 'account', :action => 'login')
         return false
       end
 
-      if @project.rss_id != params[:code].to_s.downcase and !current_user.projects.include?(@project)
-        redirect_to(home_url)
-        return false
-      end
+      return true if current_user.projects.include?(@project)
+      return false
     end
 
     true
