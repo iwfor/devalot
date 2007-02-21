28228fc6b6146d4523e4b43ef1df3ae9d85eb5b5
Peter Jones <pjones@pmade.com>
2007-02-21 15:52:53 +0000

    merge bug fixe for bug 208 to the trunk
    git-svn-id: file:///data/svn/devalot/devalot/trunk@585 3db89d08-1e44-db11-a2a8-0014220fd31b

--- test/unit/filtered_text_test.rb
+++ test/unit/filtered_text_test.rb
@@ -1,10 +1,33 @@
+################################################################################
 require File.dirname(__FILE__) + '/../test_helper'
 
+################################################################################
 class FilteredTextTest < Test::Unit::TestCase
-  fixtures :filtered_texts
+  ################################################################################
+  include PagesHelper
+  include TicketsHelper
+  include FilteredTextHelper
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  ################################################################################
+  fixtures(:filtered_texts, :projects, :pages)
+
+  ################################################################################
+  def test_cache_doesnt_whack_body
+    @project = projects(:support)
+    body = '[[index]]'
+
+    ft = FilteredText.new(:body => body.dup, :allow_caching => true)
+    assert(ft.save)
+
+    text = render_filtered_text(ft, :sanitize => false)
+    assert_equal(body, ft.body)
+    assert_equal(text, ft.body_cache)
   end
+
+  ################################################################################
+  def link_to (*args)
+    %Q(<a href="http://localhost/test/link">Link</a>)
+  end
+
 end
+################################################################################
