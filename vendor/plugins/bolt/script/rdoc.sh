11ef053e34476aefb655ea747f73ba37c6fdc927
Isaac Foraker <isaac@noscience.net>
2008-06-09 14:02:11 -0600

    Remove bolt until we're ready to integrate it.

--- vendor/plugins/bolt/script/rdoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-README=`pwd`/README
-
-rm -rf doc/rdoc
-rdoc --op doc/rdoc -A cattr_accessor --all -N \
-    --title 'Bolt Documentation' \
-    --main README README LICENSE `find . -name \*.rb`
