ac8e6156b26dcd186e821781aa8d60f99c0b7202
Isaac Foraker <isaac@noscience.net>
2008-07-10 08:56:52 -0600

    Fixed syntax errors in default-data script

--- script/default-data.rb
+++ script/default-data.rb
@@ -193,56 +193,56 @@ Policy.new(
   :name        => 'site_name', 
   :description => "The name of this installation of #APP_NAME",
   :value_type  => 'str',
-  :value       => APP_NAME,
+  :value       => APP_NAME
 ).save!
 
 Policy.new(
   :name        => 'site_description', 
   :description => "A short (one line) description for this site",
   :value_type  => 'str',
-  :value       => "Software projects that change the world",
+  :value       => "Software projects that change the world"
 ).save!
 
 Policy.new(
   :name        => 'front_page_articles', 
   :description => "The number of blog articles to display on the front page",
   :value_type  => 'int',
-  :value       => '5',
+  :value       => '5'
 ).save!
 
 Policy.new(
   :name        => 'feed_articles', 
   :description => "The number of blog articles to place in the RSS/Atom feeds",
   :value_type  => 'int',
-  :value       => '10',
+  :value       => '10'
 ).save!
 
 Policy.new(
   :name        => 'authenticator', 
   :description => 'The user account authentication system to use',
   :value_type  => 'str',
-  :value       => 'Standard',
+  :value       => 'Standard'
 ).save!
 
 Policy.new(
   :name        => 'allow_open_enrollment',
   :description => 'If the current authenticator supports it, enable self-service account creation',
   :value_type  => 'bool',
-  :value       => 'true',
+  :value       => 'true'
 ).save!
 
 Policy.new(
   :name        => 'bot_from_email',
   :description => 'The from address for mail sent by a bot that can\'t accept reply mail',
   :value_type  => 'str',
-  :value       => 'noreply@localhost.local',
+  :value       => 'noreply@localhost.local'
 ).save!
 
 Policy.new(
   :name        => 'moderation_feed_code',
   :description => 'A secret code used by RSS/Atom feed readers to access the list of moderated users',
   :value_type  => 'str',
-  :value       => Policy.random_code,
+  :value       => Policy.random_code
 ).save!
 
 Policy.new(
