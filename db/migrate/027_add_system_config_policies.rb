d38680ca862caee70f0a2eaf88fcea7af73afb66
Isaac Foraker <isaac@noscience.net>
2008-07-09 20:02:08 -0600

    Added new system configuration variables: host, port, pdf_generator

---
+++ db/migrate/027_add_system_config_policies.rb
@@ -0,0 +1,29 @@
+class AddSystemConfigPolicies < ActiveRecord::Migration
+  def self.up
+    Policy.new(
+      :name => 'host',
+      :description => 'Domain name for emailed URLs',
+      :value => 'fill.me.in',
+      :value_type => 'str'
+    ).save!
+    Policy.new(
+      :name => 'port',
+      :description => 'Port for emailed URLs',
+      :value => '80',
+      :value_type => 'int'
+    ).save!
+    Policy.new(
+      :name => 'pdf_generator',
+      :description => 'Enable PDF generator.  Requires HTMLDOC command line tool installed.',
+      :value => '80',
+      :value_type => 'bool'
+    ).save!
+  end
+
+  def self.down
+    ('host', 'port', 'pdf_generator').each do |policy|
+      x = Policy.find(:first, :conditions => { :policy_type => nil, :name => policy })
+      x.destroy unless x.blank?
+    end
+  end
+end
