89c6bba1ef3ad7bb71ad77153298e9dcb6150e74
Isaac Foraker <isaac@noscience.net>
2008-07-11 22:16:36 -0600

    Added watch_emails policy to enable watch button

--- db/migrate/027_add_system_config_policies.rb
+++ db/migrate/027_add_system_config_policies.rb
@@ -2,26 +2,32 @@ class AddSystemConfigPolicies < ActiveRecord::Migration
   def self.up
     Policy.new(
       :name => 'host',
-      :description => 'Domain name for emailed URLs',
+      :description => 'Domain name.  Needed for emailed URLs.',
       :value => 'fill.me.in',
       :value_type => 'str'
     ).save!
     Policy.new(
       :name => 'port',
-      :description => 'Port for emailed URLs',
+      :description => 'Port.  Needed for emailed URLs',
       :value => '80',
       :value_type => 'int'
     ).save!
     Policy.new(
       :name => 'pdf_generator',
       :description => 'Enable PDF generator.  Requires HTMLDOC command line tool installed.',
-      :value => '80',
+      :value => 'false',
+      :value_type => 'bool'
+    ).save!
+    Policy.new(
+      :name => 'watch_emails',
+      :description => 'Enable watch emailer.  Requires adding script/helpers/cron_email.rb to crontab.',
+      :value => 'false',
       :value_type => 'bool'
     ).save!
   end
 
   def self.down
-    ['host', 'port', 'pdf_generator'].each do |policy|
+    ['host', 'port', 'pdf_generator', 'watch_emails'].each do |policy|
       x = Policy.find(:first, :conditions => { :policy_type => nil, :name => policy })
       x.destroy unless x.blank?
     end
